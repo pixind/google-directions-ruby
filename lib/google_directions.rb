cedc2d82110fa81d4726f2af40e2fd34b0a75b4e
txarli <gabriel@pixind.com>
2017-05-05 17:45:57 +0200

    Adds european measures.

--- lib/google_directions.rb
+++ lib/google_directions.rb
@@ -1,26 +1,27 @@
 # encoding: UTF-8
+
 require 'cgi'
 require 'net/http'
 require 'open-uri'
 require 'nokogiri'
 
+# Base gem implementation
 class GoogleDirections
-
   attr_reader :status, :doc, :xml, :origin, :destination, :options
 
   @@base_url = 'http://maps.googleapis.com/maps/api/directions/xml'
 
   @@default_options = {
-    :language => :en,
-    :alternative => :true,
-    :sensor => :false,
-    :mode => :driving,
+    language: :en,
+    alternative: :true,
+    sensor: :false,
+    mode: :driving,
   }
 
-  def initialize(origin, destination, opts=@@default_options)
+  def initialize(origin, destination, opts = @@default_options)
     @origin = origin
     @destination = destination
-    @options = opts.merge({:origin => @origin, :destination => @destination})
+    @options = opts.merge(origin: @origin, destination: @destination)
 
     @url = @@base_url + '?' + @options.to_query
     @xml = open(@url).read
@@ -36,11 +37,20 @@ class GoogleDirections
   #http://maps.google.com/maps/api/directions/xml?origin=St.+Louis,+MO&destination=Nashville,+TN&sensor=false&key=ABQIAAAAINgf4OmAIbIdWblvypOUhxSQ8yY-fgrep0oj4uKpavE300Q6ExQlxB7SCyrAg2evsxwAsak4D0Liiv
 
   def drive_time_in_minutes
-    if @status != "OK"
+    if @status != 'OK'
       drive_time = 0
     else
-      drive_time = @doc.css("duration value").last.text
+      drive_time = @doc.css('duration value').last.text
       convert_to_minutes(drive_time)
+    end
+  end
+
+  def drive_time_in_seconds
+    if @status != 'OK'
+      drive_time = 0
+    else
+      drive_time = @doc.css('duration value').last.text.to_f
+      drive_time
     end
   end
 
@@ -50,26 +60,36 @@ class GoogleDirections
     unless @status == 'OK'
       @distance = 0
     else
-      @distance = @doc.css("distance value").last.text
+      @distance = @doc.css('distance value').last.text
     end
   end
 
   def distance_text
     return @distance_text if @distance_text
     unless @status == 'OK'
-      @distance_text = "0 km"
+      @distance_text = '0 km'
     else
-      @distance_text = @doc.css("distance text").last.text
+      @distance_text = @doc.css('distance text').last.text
     end
   end
 
   def distance_in_miles
-    if @status != "OK"
+    if @status != 'OK'
       distance_in_miles = 0
     else
       meters = distance
       distance_in_miles = (meters.to_f / 1610.22).round
       distance_in_miles
+    end
+  end
+
+  def distance_in_kilometers
+    if @status != 'OK'
+      distance_in_kilometers = 0
+    else
+      meters = distance
+      distance_in_kilometers = (meters.to_f / 1000).round
+      distance_in_kilometers
     end
   end
 
@@ -101,5 +121,3 @@ class Hash
   end unless method_defined? :to_query
 
 end
-
-
